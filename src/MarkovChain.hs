7b5859c3dd02791dd4f394c1ee591b713b2a925a
Dilawar Singh <dilawars@ncbs.res.in>
2016-08-05 11:45:28 +0530

    Some time in free time. Back to python version.

--- src/MarkovChain.hs
+++ src/MarkovChain.hs
@@ -3,6 +3,14 @@ module MarkovChain
     markov
     ) where
 
+import Numeric.LinearAlgebra
+import Text.ParserCombinators.Parsec
 
-{-markovChain :: [ String ] --}
-markov mat = mat
+matParse = sepBy line (char ';') 
+line = sepBy cell (char ',' )
+cell = many ( noneOf ",\n;" )
+
+stringToTranstionMatrix :: String -> Either ParseError [[String]]
+stringToTranstionMatrix str = parse matParse "unknown" str
+
+markov matStr = stringToTranstionMatrix matStr
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,6 @@ import MarkovChain
 
 main :: IO ()
 main = do
-    let a = markov "a b c"
+    let a = markov "0.1, 0.1;0.2  ,0.2;0.1,0.2"
     print $ a
     putStrLn "Test suite not yet implemented"
